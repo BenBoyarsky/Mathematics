426a0b18f12d09586009e17f2a9e7f5078bf55b2
Benjamin <benjaminboyarsky21@gmail.com>
2026-09-23 23:35:45 -0700

    Reduce under each pivot

--- row_reducer.py
+++ row_reducer.py
@@ -1,29 +1,51 @@
 import numpy as np
 
+#Assume A has one solution  --> convenient pivots
 def reduce(A):
     M = A.astype(float)
     M_next = M.copy()
     num_rows = M.shape[0]
     num_cols = M.shape[1]
 
-    #get a number to M11
-    if M[0, 0] == 0:
-        for i in range(1, num_rows):
-            if not M[i, 0] == 0:
-                row1 = M[0]
-                row2 = M[i]
-                M_next[0] = row2
-                M_next[i] = row1
-                break
+    #reduce each column below pivot
+    for j in range(num_cols - 1): 
+        #get a nonzero num to pivot
+        pivot = (j, j)
+        if M_next[pivot] == 0:
+            for i in range(j+1, num_rows):
+                if not M_next[i, j] == 0:
+                    rowA = M_next[j].copy()
+                    rowB = M_next[i].copy()
+                    M_next[j] = rowB
+                    M_next[i] = rowA
+                    break
+        #normalize pivot
+        M_next[j] /= M_next[pivot]
 
-    #subtract a * r1 from each row to make each row have 0 for the first position
-    for i in range(1, num_rows):
-        a = (-1) * (M_next[i, 0] / M_next[0, 0])
-        M_next[i] += a * M_next[0]
+        #put zeros under pivot
+        for i in range(j+1, num_rows):
+            a = (-1) * (M_next[i, j] / M_next[pivot])
+            M_next[i] += a * M_next[j]
+
+        print(M_next, '\n')
+
+    #reduce each column 
+    #for j in range(num_cols-2:-1:-1):
+        
  
     return M_next
 
-A = np.array([[0, 3, -1, 4],
-             [1, 2, 3, 3],
-             [5, 0, 2, 0]])
+A = np.array([
+
+    [2,  1, -1,  0,  3,  10],
+
+    [1, -2,  2,  1, -1,  -3],
+
+    [3,  1,  1, -2,  2,  12],
+
+    [1,  3, -1,  2,  1,   7],
+
+    [2, -1,  3,  1,  2,   8]
+
+])
 print(reduce(A))
