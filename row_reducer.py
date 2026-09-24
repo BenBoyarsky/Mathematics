ec7765f6ea39781a2faa00bb979b93a27a7d1325
Benjamin <benjaminboyarsky21@gmail.com>
2026-09-24 14:37:19 -0700

    Added rank()

--- row_reducer.py
+++ row_reducer.py
@@ -34,11 +34,29 @@ def reduce(A):
         for i in range(j-1,-1,-1):
             a = -1 * M_next[i, j]
             M_next[i] += a * M_next[j]
-        
  
     return M_next
 
-A = np.array([
+#assumes RR augmented matrix
+def rank(A):
+    M = A.copy()
+    rows = M.shape[0]
+    cols = M.shape[1]
+    pivots = 0
+    i = 0
+    j = 0
+    while i < rows and j < cols:
+        if M[i, j] == 1:
+            pivots += 1
+            i += 1
+            j += 1
+        else:
+            j += 1
+
+    return pivots
+            
+
+A1 = np.array([
 
     [2,  1, -1,  0,  3,  10],
 
@@ -51,4 +69,17 @@ A = np.array([
     [2, -1,  3,  1,  2,   8]
 
 ])
-print(reduce(A))
+#print(reduce(A1))
+
+A = np.array([
+
+    [1, 0, 3, 0],
+
+    [0, 1, 2, 0],
+
+    [0, 0, 0, 1]
+
+])
+
+print(rank(A))
+
